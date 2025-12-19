65d1e5527dbb03a13d1e3d0624da25412b26c563
Limpolits <iliasfits@outlook.com>
2025-12-19 18:36:13 +0200

    Add project files.

---
+++ EmployeeAPII/Class1.cs
@@ -0,0 +1,6 @@
+﻿public class Employee
+{
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
